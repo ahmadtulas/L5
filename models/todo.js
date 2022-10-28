4c92eeeaab4331ac214ee169eb85d8b83af2baf7
Ahmad Jamal <ahmadjamalcs@gmail.com>
2022-10-28 14:51:34 +0530

    displayable function imporvement

--- models/todo.js
+++ models/todo.js
@@ -81,9 +81,11 @@ module.exports = (sequelize, DataTypes) => {
 
     displayableString() {
       let checkbox = this.completed ? "[x]" : "[ ]";
-      return `${
-        this.id
-      }. ${checkbox} ${this.title.trim()} ${this.dueDate.trim()}`;
+      return `${this.id}. ${checkbox} ${this.title.trim()} ${
+        this.dueDate == new Date().toLocaleDateString("en-CA")
+          ? ""
+          : this.dueDate
+      }`.trim();
     }
   }
   Todo.init(
